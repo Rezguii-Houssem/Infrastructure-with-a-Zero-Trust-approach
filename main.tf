79a6d37e4a04907f1bcbcfa76589ea3bcfcd7e42
houssi <rezguii-houssem@gmail.com>
2026-02-10 00:59:44 +0100

    creating an s3

--- main.tf
+++ main.tf
@@ -8,6 +8,28 @@ terraform {
   }
 }
 
+resource "aws_s3_bucket" "main" {
+    bucket = "the-alpha-project-terraform-stateeee" # Your bucket name
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+    bucket = aws_s3_bucket.main.id
+
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
+}
+
+resource "aws_s3_bucket_versioning" "main" {
+    bucket = aws_s3_bucket.main.id
+
+    versioning_configuration {
+        status = "Enabled"
+    }
+}
+
 resource "terraform_data" "workspace_guard" {
     lifecycle {
       precondition {
